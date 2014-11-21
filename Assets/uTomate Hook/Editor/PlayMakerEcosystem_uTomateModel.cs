c7a4c0d8c383991273e3024fab96b05dff58b476
jeanfabre <contact@fabrejean.net>
2014-11-21 16:13:56 +0300

    initial commit

---
+++ Assets/uTomate Hook/Editor/PlayMakerEcosystem_uTomateModel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayMakerEcosystem_uTomateModel : UTNodeEditorModel {
+	
+	public UTNode GetNodeForEntry(UTAutomationPlanEntry entry) {
+		return Graph.GetNodeFor(entry); 
+	}
+	
+	public UTAutomationPlanEntry AddAction(UTAction action) {
+		return AddAction(action,Vector2.zero);
+	}
+
+	public UTAutomationPlanEntry AddAction(UTAction action, Vector2 position) {
+		var entry = AddEntry<UTAutomationPlanSingleActionEntry>(position);
+		entry.action = action;
+		return entry;
+	}
+	
+	public void Connect(UTAutomationPlanEntry firstEntry, UTAutomationPlanEntry secondEntry) {
+		var firstNode = Graph.GetNodeFor(firstEntry);
+		var secondNode = Graph.GetNodeFor(secondEntry);
+		
+		// find the "nextEntry" connector of the first node
+		UTNode.Connector nextEntryConnector = null; 
+		foreach(var connector in firstNode.Connectors) {
+			if (connector.property == "nextEntry") {
+				nextEntryConnector = connector;
+				break;
+			}
+		}
+		if (nextEntryConnector != null) {
+			AddConnection(nextEntryConnector, secondNode);
+		}
+	}
+
+}
---
+++ Assets/uTomate Hook/Editor/PlayMakerEcosystem_uTomatePlanBuilder.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PlayMakerEcosystem_uTomatePlanBuilder : EditorWindow {
+
+	[MenuItem ("PlayMaker/Addons/Ecosystem/Build uTomate Plan")]
+	public static void CreateEcosystemPlan()
+	{
+	// get the folder selection
+
+	// create a new plan at the currently selected folder.
+		UTAutomationPlan plan = UTils.CreateAssetOfType<UTAutomationPlan>("name");
+	
+	// create a new instance of the new class we created above.
+	var editorModel = new PlayMakerEcosystem_uTomateModel();
+
+	editorModel.LoadPlan(plan);
+	
+		Selection.activeInstanceID = plan.GetInstanceID();
+
+	// now you can create actions.
+
+	// FIRST ACTION
+		UTEchoAction echoAction = UTAction.Create<UTEchoAction>();
+
+	// set their properties
+	echoAction.text.Value = "Hello World";
+	echoAction.text.UseExpression = false; // this toggles f(x)
+	
+	// add the action to the automation plan
+	var echoActionEntry = editorModel.AddAction(echoAction);
+		echoActionEntry.name = "wtf";
+
+		Selection.activeInstanceID = plan.GetInstanceID();
+
+	// SECOND ACTION
+	UTEchoAction anotherAction = UTAction.Create<UTEchoAction>(); 
+
+
+	// set their properties
+	anotherAction.text.Value = "double dva";
+	anotherAction.text.UseExpression = false; // this toggles f(x)
+
+	// add it as well
+	var anotherActionEntry = editorModel.AddAction(anotherAction);
+	
+	
+	//CONNECT
+	// now connect the first echo action to the other action using the Connect method we wrote
+	editorModel.Connect(echoActionEntry, anotherActionEntry);
+	
+	// finally set the echo action as first action
+	var echoActionNode = editorModel.Graph.GetNodeFor(echoActionEntry);
+	editorModel.SetFirstNode(echoActionNode);
+	
+
+	// if you want you can beautify the graph
+	// so not all nodes are located at (0,0)
+	editorModel.RelayoutPlan();
+	
+	// finally you can execute your plan using 
+	UTomate.Run(plan);
+	}
+}
