517dd019d16c98cfc66578539257e331550c47de
Jean Fabre <contact@fabrejean.net>
2017-12-11 09:44:21 +0300

    BetterTouchTool for IOS TouchBar

--- Assets/net.fabrejean/Editor/TouchBar/TouchBarProxy.cs
+++ Assets/net.fabrejean/Editor/TouchBar/TouchBarProxy.cs
@@ -1,28 +1,37 @@
-﻿
-using System;
+﻿using System;
 using UnityEditor;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-
-namespace Net.FabreJean.PlayMaker.Ecosystem
+namespace Net.FabreJean.Ecosystem
 {
+	/// <summary>
+	/// Touch bar proxy.
+	/// Implemented various menu items with shortcuts to bridge functions easily with External Devices, like the TouchBar. 
+	/// These shortcuts so not exists built in Unity nor PlayMaker thus this proxy. All other features that already exists as a menu item WITH shortcut can be accessed as is already,
+	/// </summary>
 	public class TouchBarProxy : EditorWindow {
 		
 		[MenuItem ("PlayMaker/Addons/TouchBar/FsmEditor Maximize Toggle %&m",false)]
-		static void toggleFmsEditorMaximize () {
+		static void ToggleFmsEditorMaximize () {
 			#if PLAYMAKER	
-				System.Reflection.Assembly assembly = typeof(HutongGames.PlayMakerEditor.FsmEditorWindow).Assembly;
-				Type type = assembly.GetType("HutongGames.PlayMakerEditor.FsmEditorWindow");
-				EditorWindow gameview = EditorWindow.GetWindow(type);
-
-				gameview.maximized = !gameview.maximized;
-
+			System.Reflection.Assembly assembly = typeof(HutongGames.PlayMakerEditor.FsmEditorWindow).Assembly;
+			Type type = assembly.GetType("HutongGames.PlayMakerEditor.FsmEditorWindow");
+			EditorWindow gameview = EditorWindow.GetWindow(type);
+			
+			gameview.maximized = !gameview.maximized;
+			
 			#else
-				Debug.Log("PlayMaker is not installed. Function has no effect");
+			Debug.Log("PlayMaker is not installed. Function has no effect");
 			#endif
 		}
-
+		
+		[MenuItem ("PlayMaker/Addons/TouchBar/Import Package %&i",false)]
+		static void ImportCustomPackage () {
+			
+			EditorApplication.ExecuteMenuItem ("Assets/Import Package/Custom Package...");
+		}
+		
 	}
 }
